dato/algo2-corrector

commit 467059ba39ee00d4d0c43105e24525d8ddb2a181

Simplificar save_output() usando path.write_bytes() y format strings.

corrector.py
@@ -322,8 +322,7 @@ class Moss:
 
   def save_output(self, output):
     filepath = self._dest / "README.md"
-    with open(filepath,"w") as f:
-      f.write("```\n"+output+"```")
+    filepath.write_bytes(f"```\n{output}```")
     return self._git(["add", filepath]) == 0
 
   def commit_emoji(self, output):

commit 2f7f035842cb08b476f7bf56f826889115911b27

Usar path.write_text() y no write_bytes(), output es cadena.
Fixes: 467059b “Simplificar save_output() usando path.write_bytes()
y format strings.”

corrector.py
@@ -323,7 +323,7 @@ class Moss:
 
   def save_output(self, output):
     filepath = self._dest / "README.md"
-    filepath.write_bytes(f"```\n{output}```")
+    filepath.write_text(f"```\n{output}```")
     return self._git(["add", filepath]) == 0
 
   def commit_emoji(self, output):